870e696b00f33d40fe5cc86419a450d8be36dda6
Xiang Liu <xiang.liu@rightcapital.com>
2017-10-26 19:02:40 +0800

    Restful

---
+++ src/User.php
@@ -0,0 +1,64 @@
+<?php
+
+// src/User.php
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @Entity @Table(name="users")
+ */
+class User
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @OneToMany(targetEntity="Bug", mappedBy="reporter")
+     * @var Bug[] An ArrayCollection of Bug objects.
+     **/
+    protected $reportedBugs;
+
+    /**
+     * @OneToMany(targetEntity="Bug", mappedBy="engineer")
+     * @var Bug[] An ArrayCollection of Bug objects.
+     **/
+    protected $assignedBugs;
+
+    public function addReportedBug(Bug $bug)
+    {
+        $this->reportedBugs[] = $bug;
+    }
+
+    public function assignedToBug(Bug $bug)
+    {
+        $this->assignedBugs[] = $bug;
+    }
+
+    public function __construct()
+    {
+        $this->reportedBugs = new ArrayCollection();
+        $this->assignedBugs = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
